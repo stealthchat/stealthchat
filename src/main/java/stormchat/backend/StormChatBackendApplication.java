471c5428a52f50033eb3a2ce161e1001146b9ce0
mxh <maximilian.hilmbauer@outlook.com>
2022-12-02 22:12:17 +0100

    init

--- src/main/java/stormchat/backend/StormChatBackendApplication.java
+++ src/main/java/stormchat/backend/StormChatBackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StormChatBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StormChatBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StormChatBackendApplication.class, args);
+    }
 
 }
